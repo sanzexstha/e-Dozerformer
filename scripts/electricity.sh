0e8edd47dad404fdf2a04a59444c5d96ef5bd58e
Sanjeev Shrestha <hellosanjeev4@gmail.com>
2026-02-01 23:44:32 -0600

    changed the electrcity bash

--- scripts/electricity.sh
+++ scripts/electricity.sh
@@ -12,25 +12,25 @@ vary_len=1
 for seed in ${seeds[@]}
 do
     #----------------------------------predict length 96---------------------------------------
-    python run.py --seed $seed --data electricity --model $model --moving_avg '13, 17' \
+    python run.py --seed $seed --data electricity_labeled --model $model --moving_avg '13, 17' \
     --seq_len 720 --label_len 96 --pred_len 96 --embed_dim 8 \
     --learning_rate 1e-3 --patch_size $patch_size \
     --local_window $local_window --stride $stride --vary_len $vary_len
 
     #----------------------------------predict length 192---------------------------------------
-    python run.py --seed $seed --data electricity --model $model --moving_avg '13, 17' \
+    python run.py --seed $seed --data electricity_labeled --model $model --moving_avg '13, 17' \
     --seq_len 720 --label_len 96 --pred_len 192 --embed_dim 8 \
     --learning_rate 1e-3 --patch_size $patch_size \
     --local_window $local_window --stride $stride --vary_len $vary_len
 
     #----------------------------------predict length 336---------------------------------------
-    python run.py --seed $seed --data electricity --model $model --moving_avg '13, 17' \
+    python run.py --seed $seed --data electricity_labeled --model $model --moving_avg '13, 17' \
     --seq_len 720 --label_len 96 --pred_len 336 --embed_dim 8 \
     --learning_rate 1e-3 --patch_size $patch_size \
     --local_window $local_window --stride $stride --vary_len $vary_len
 
     #----------------------------------predict length 720---------------------------------------
-    python run.py --seed $seed --data electricity --model $model --batch_size 16 --moving_avg '13, 17' \
+    python run.py --seed $seed --data electricity_labeled --model $model --batch_size 16 --moving_avg '13, 17' \
     --seq_len 720 --label_len 96 --pred_len 720 --embed_dim 8 \
     --learning_rate 1e-3 --patch_size $patch_size \
     --local_window $local_window --stride $stride --vary_len $vary_len
